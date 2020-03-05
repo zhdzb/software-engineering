273fd329cc0a9f0d37a6c5d6105adb8a10c75093
zhdzb <591893517@qq.com>
2020-03-05 13:39:39 +0800

    kechengshijian_2_gerenbaincheng_1

---
+++ code/个人编程1.java
@@ -0,0 +1,50 @@
+package sorftware_engineering;
+import java.io.*;
+
+public class e_2 {
+    public static void main(String[] args) {
+        String in_path = "C:\\Users\\59189\\Desktop\\software-engineering\\yq_in.txt";
+        String out_path = "C:\\Users\\59189\\Desktop\\software-engineering\\my_yq_out.txt";
+        try {
+            //读文件流
+            FileInputStream fin = new FileInputStream(in_path);
+            InputStreamReader reader = new InputStreamReader(fin,"GBK");
+            BufferedReader buffreader = new BufferedReader(reader);
+            //写文件流
+            FileOutputStream fout = new FileOutputStream(out_path);
+            OutputStreamWriter writer = new OutputStreamWriter(fout,"GBK");
+            BufferedWriter buffwriter = new BufferedWriter(writer);
+            //处理txt
+            String temp = "";
+            String title = "";
+            while((temp = buffreader.readLine()) != null){
+                //待明确地区不记录
+                if(temp.contains("待明确地区")) continue;
+                //分割字符串中省份与其他信息
+                String head = temp.substring(0,2);
+                String msg = temp.substring(3);
+                if (title.equals(head) != true) {
+                    //单独输出省份
+                    if(title != ""){
+                        //System.out.println(title);
+                        buffwriter.write("\r\n");
+                    }
+                    buffwriter.write(head.substring(0,2) + "\r\n");
+                    title = head;
+                }
+                //输出剩余信息
+                buffwriter.write(msg.trim() + "\r\n");
+                buffwriter.flush();
+            }
+            //结束
+            fin.close();
+            fout.close();
+            reader.close();
+            writer.close();
+            buffreader.close();
+            buffwriter.close();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
